c073d3050102c862c1e0769ec717ab14be031fd2
WickedBrat <harshsrivastav123@gmail.com>
2019-04-29 01:39:04 +0530

    Added files for initial email sending

--- index.js
+++ index.js
@@ -3,6 +3,45 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const nodemailer = require("nodemailer"),
+  creds = require("./creds"),
+  transporter = nodemailer.createTransport({
+    host: "smtp.zoho.eu",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "hello@cynthesize.co",
+      pass: process.env("EMAIL_PASS")
+    }
+  }),
+  EmailTemplate = require("email-templates").EmailTemplate,
+  path = require("path"),
+  Promise = require("bluebird");
+
+function sendEmail(obj) {
+  return transporter.sendMail(obj);
+}
+
+function loadTemplate(templateName, contexts) {
+  let template = new EmailTemplate(
+    path.join(__dirname, "templates", templateName)
+  );
+  return Promise.all(
+    contexts.map(context => {
+      return new Promise((resolve, reject) => {
+        template.render(context, (err, result) => {
+          if (err) reject(err);
+          else
+            resolve({
+              email: result,
+              context
+            });
+        });
+      });
+    })
+  );
+}
+
 function echo(event) {
   console.log(event);
   let responseBody = "";
@@ -10,6 +49,27 @@ function echo(event) {
     responseBody = `New user ${event.data.new.id} inserted, with data: ${
       event.data.new.name
     }`;
+
+    loadTemplate("welcome", [event.data])
+      .then(results => {
+        return Promise.all(
+          results.map(result => {
+            sendEmail({
+              to: result.context.email,
+              from: "hello@cynthesize.co",
+              subject: result.email.subject,
+              html: result.email.html,
+              text: result.email.text
+            });
+          })
+        );
+      })
+      .then(() => {
+        console.log("Yay!");
+      })
+      .catch(e => {
+        console.log("Error Found: ", e);
+      });
   } else if (event.op === "UPDATE") {
     responseBody = `User ${event.data.new.id} updated, with data: ${
       event.data.new.name
