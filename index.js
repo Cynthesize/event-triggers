341dab5b5d2104941988bbdef62388e641d8db93
strikertanmay <tanmaymittal0@gmail.com>
2019-06-28 01:15:14 +0530

    add project event trigger added

--- index.js
+++ index.js
@@ -2,165 +2,166 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const nodemailer = require("nodemailer"),
-    transporter = nodemailer.createTransport({
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        auth: {
-            user: 'tanmaymittal0@gmail.com',
-            pass: 'legiony520'
-        }
-    }),
-    EmailTemplate = require("email-templates").EmailTemplate,
-    path = require("path"),
-    Promise = require("bluebird");
+  transporter = nodemailer.createTransport({
+    host: "smtp.zoho.in",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "hello@cynthesize.co",
+      pass: process.env.EMAIL_PASS
+    }
+  }),
+  EmailTemplate = require("email-templates").EmailTemplate,
+  path = require("path"),
+  Promise = require("bluebird");
 app.use(bodyParser.json());
 
 function sendEmail(obj) {
-    return transporter.sendMail(obj);
+  return transporter.sendMail(obj);
 }
 
 function loadTemplate(templateName, contexts) {
-    let template = new EmailTemplate(
-        path.join(__dirname, "templates", templateName)
-    );
+  let template = new EmailTemplate(
+    path.join(__dirname, "templates", templateName)
+  );
 
-    return Promise.all(
-        contexts.map(context => {
-            return new Promise((resolve, reject) => {
-                template.render(context.new, (err, result) => {
-                    if (err) reject(err);
-                    else
-                        resolve({
-                            email: result,
-                            context
-                        });
-                });
+  return Promise.all(
+    contexts.map(context => {
+      return new Promise((resolve, reject) => {
+        template.render(context.new, (err, result) => {
+          if (err) reject(err);
+          else
+            resolve({
+              email: result,
+              context
             });
-        })
-    );
+        });
+      });
+    })
+  );
 }
 
-function echo(event) {
+function echo(payload) {
 
-    if (event === event1) {
-        let responseBody = "";
+  if (payload.trigger.name === "test") {
+    let responseBody = "";
 
-        if (event1.op === "INSERT") {
-            responseBody = `New user ${event1.data.new.id} inserted, with data: ${
-                event1.data.new.name
-                }`;
+    if (payload.event.op === "INSERT") {
+      responseBody = `New user ${payload.event.data.new.id} inserted, with data: ${
+        payload.event.data.new.name
+        }`;
 
-            loadTemplate("welcome", [event1.data])
-                .then(results => {
-                    return Promise.all(
-                        results.map(result => {
-                            sendEmail({
-                                to: result.context.new.email,
-                                from: "hello@cynthesize.co",
-                                subject: result.email.subject,
-                                html: result.email.html,
-                                text: result.email.text
-                            });
-                        })
-                    );
-                })
-                .catch(e => {
-                    console.log("Error Found: ", e);
-                });
-        } else if (event1.op === "UPDATE") {
-            responseBody = `User ${event1.data.new.id} updated, with data: ${
-                event1.data.new.name
-                }`;
-        } else if (event1.op === "DELETE") {
-            responseBody = `User ${event1.data.old.id} deleted, with data: ${
-                event1.data.old.name
-                }`;
-        }
-
-        return responseBody;
+      loadTemplate("welcome", [payload.event.data])
+        .then(results => {
+          return Promise.all(
+            results.map(result => {
+              sendEmail({
+                to: result.context.new.email,
+                from: "hello@cynthesize.co",
+                subject: result.email.subject,
+                html: result.email.html,
+                text: result.email.text
+              });
+            })
+          );
+        })
+        .catch(e => {
+          console.log("Error Found: ", e);
+        });
+    } else if (payload.event.op === "UPDATE") {
+      responseBody = `User ${payload.event.data.new.id} updated, with data: ${
+        payload.event.data.new.name
+        }`;
+    } else if (payload.event.op === "DELETE") {
+      responseBody = `User ${payload.event.data.old.id} deleted, with data: ${
+        payload.event.data.old.name
+        }`;
     }
 
-    else if (event === event2) {
-        let responseBody = "";
+    return responseBody;
+  }
 
-        if (event2.op === "INSERT") {
-            responseBody = `New project ${event2.data.new.id} inserted, with data: ${
-                event2.data.new.project_name
-                }`;
+  else if (payload.trigger.name === "add_project") {
+    let responseBody = "";
 
-            loadTemplate("Add_Project", [event2.session_variables])
-                .then(results => {
-                    return Promise.all(
-                        results.map(result => {
-                            sendEmail({
-                                to: result.context.x - hasura - email,
-                                from: "hello@cynthesize.co",
-                                subject: result.email.subject,
-                                html: result.email.html,
-                                text: result.email.text
-                            });
-                        })
-                    );
-                })
-                .catch(e => {
-                    console.log("Error Found: ", e);
-                });
-        } else if (event2.op === "UPDATE") {
-            responseBody = `User ${event2.data.new.id} updated, with data: ${
-                event2.data.new.project_name
-                }`;
-        } else if (event2.op === "DELETE") {
-            responseBody = `User ${event2.data.old.id} deleted, with data: ${
-                event2.data.old.project_name
-                }`;
-        }
+    if (payload.event.op === "INSERT") {
+      responseBody = `New project ${payload.event.data.new.id} inserted, with data: ${
+        payload.event.data.new.project_name
+        }`;
 
-        return responseBody;
+      loadTemplate("Add_Project", [payload.event.session_variables])
+        .then(results => {
+          return Promise.all(
+            results.map(result => {
+              sendEmail({
+                to: result.context.x-hasura-email,
+                from: "hello@cynthesize.co",
+                subject: result.email.subject,
+                html: result.email.html,
+                text: result.email.text
+              });
+            })
+          );
+        })
+        .catch(e => {
+          console.log("Error Found: ", e);
+        });
+    } else if (payload.event.op === "UPDATE") {
+      responseBody = `User ${payload.event.data.new.id} updated, with data: ${
+        payload.event.data.new.project_name
+        }`;
+    } else if (event.op === "DELETE") {
+      responseBody = `User ${payload.event.data.old.id} deleted, with data: ${
+        payload.event.data.old.project_name
+        }`;
     }
+
+    return responseBody;
+  }
 
 }
 
 
 app.post("/", function (req, res) {
-    try {
-        let event1 = req.body.event;
-        let result = echo(event1);
+  try {
+    let payload = req.body;
+    let result = echo(payload);
 
-        res.json(result);
-    } catch (e) {
+    res.json(result);
+  } catch (e) {
 
-        console.log(e);
-        res.status(500).json(e.toString());
-    }
+    console.log(e);
+    res.status(500).json(e.toString());
+  }
 });
 
 app.post("/addproject", function (req, res) {
-    try {
-        let event2 = req.body.event;
-        let result1 = echo(event2);
+  try {
+    let payload = req.body;
+    let result1 = echo(payload);
 
-        res.json(result1);
-        // console.log(res);
-    } catch (e) {
+    res.json(result1);
+    // console.log(res);
+  } catch (e) {
 
-        console.log(e);
-        res.status(500).json(e.toString());
-    }
+    console.log(e);
+    res.status(500).json(e.toString());
+  }
 });
 
 app.get("/", function (req, res) {
-    res.send("Hello World - For Event Triggers, try a POST request?");
+  res.send("Hello World - For Add User Event Triggers, try a POST request?");
 });
 
 app.get("/addproject", function (req, res) {
-    res.send("Hello World - For Event Triggers, try a POST request?");
+  res.send("Hello World - For Add Project Event Triggers, try a POST request?");
 
 });
 
-let server = app.listen(process.env.PORT || 5000, function (err) {
-    if (!err) {
-        console.log("Server Listening on PORT " + server.address().port);
-    } else {
-        console.log(err);
-    }
+let server = app.listen(process.env.PORT || 5000, function(err) {
+  if(!err) {
+    console.log("Server Listening on PORT " + server.address().port);
+  } else {
+    console.log(err);
+  }
 });
